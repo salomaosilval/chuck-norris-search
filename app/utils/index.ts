c7c8884873b65537f9c1c5e970c2569aae084f3c
salomaosilval <salomaosilvadev@gmail.com>
2025-03-10 15:40:13 -0400

    feat: add date formatting utility function

---
+++ app/utils/index.ts
@@ -0,0 +1,9 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+};
